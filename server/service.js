77810aa9657f9aec796fd8b426caea9ba8c94084
AntonL-git <alyudchik@mail.ru>
2023-09-01 00:04:30 +0200

    extract api logic

--- server/service.js
+++ server/service.js
@@ -1,95 +1,87 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
-const port = 8080
+const app = express();
+const port = 8080;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-//Initial data
+// Initial data
 let todos = [
-    {title: "Finish the ticket", completed: false, id:1},
-    {title: "Do the homework", completed: false, id:2},
-    {title: "Submit application", completed: false, id:3}
-]
+  { title: 'Finish the ticket', completed: false, id: 1 },
+  { title: 'Do the homework', completed: false, id: 2 },
+  { title: 'Submit application', completed: false, id: 3 },
+];
 
-app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
 
-//Check todo fields
-function validateTodo(req, res, next){
-    if (req.is('application/json')) {
-        const {title, completed}  = req.body;
-        if (typeof title !== 'undefined' && typeof completed !== 'undefined') {
-          return next();
-        } else {
-          return res.status(400).json({ error: 'Invalid Todo, check the fields' });
-        }
-      } else {
-        return res.status(400).json({ error: 'Invalid Content-Type, JSON expected' });
-      }
+// Check todo fields
+function validateTodo(req, res, next) {
+  if (req.is('application/json')) {
+    const { title, completed } = req.body;
+    if (typeof title !== 'undefined' && typeof completed !== 'undefined') {
+      return next();
+    }
+    return res.status(400).json({ error: 'Invalid Todo, check the fields' });
+  }
+  return res.status(400).json({ error: 'Invalid Content-Type, JSON expected' });
 }
 
 // Get all todos
-app.get('/todo', (req,res) => {
-    res.send(todos)
-})
+app.get('/todo', (req, res) => {
+  res.send(todos);
+});
 
 // Create a new todo
-app.post('/todo', validateTodo, (req,res) => {
-    const {title, completed} = req.body
-    current_index = todos.length === 0 ? 0 : Math.max(...todos.map(task => task.id))
+app.post('/todo', validateTodo, (req, res) => {
+  const { title, completed } = req.body;
+  currentIndex = todos.length === 0 ? 0 : Math.max(...todos.map((task) => task.id));
 
-    const newTodo = {
-        id: ++current_index,
-        title, 
-        completed
+  const newTodo = {
+    id: ++currentIndex,
+    title,
+    completed,
+  };
+
+  todos.push(newTodo);
+  console.log('Created Todo', todos);
+  res.send(todos);
+});
+
+// Update Todo's status or title
+app.put('/todo/:id', (req, res) => {
+  const { id } = req.params;
+
+  const updatedTodos = todos.map((todo) => {
+    if (todo.id == id) {
+      return {
+        ...todo,
+        completed: true,
+      };
+    }
+    return todo;
+  });
+
+  const completedTodo = updatedTodos.find((todo) => todo.id == id && todo.completed === true);
+
+  if (updatedTodos.some((todo) => todo.id == id)) {
+    // put completed todo in the end of the list
+    if (completedTodo) {
+      updatedTodos.splice(updatedTodos.indexOf(completedTodo), 1);
+      updatedTodos.push(completedTodo);
     }
 
-    todos.push(newTodo)
-    console.log("Created Todo", todos)
-    res.send(todos)
-})
+    todos = updatedTodos;
+    res.send(todos);
+  } else {
+    res.send(todos);
+  }
+});
 
-//Update Todo's status or title
-app.put('/todo/:id', (req,res) => {
-    const {id} = req.params;
-
-    const updatedTodos = todos.map(todo => {
-        if(todo.id == id){
-            return {
-                ...todo,
-                completed: true
-            }
-        }
-        return todo
-    })
-
-    const completedTodo = updatedTodos.find(todo => todo.id == id && todo.completed === true)
-
-    if(updatedTodos.some(todo => todo.id == id)){
-        // put completed todo in the end of the list
-        if(completedTodo){
-            updatedTodos.splice(updatedTodos.indexOf(completedTodo), 1);
-            updatedTodos.push(completedTodo);
-        }
-
-        todos=updatedTodos
-        res.send(todos)
-    } else {
-        res.send(todos)
-    }
-})
-
-//Delete Todo by id
-app.delete('/todo/:id', (req, res) =>{
-    const {id} = req.params
-    todos = todos.filter(task => task.id != id)
-    res.send(todos)
-})
-
-
-
-
-
- 
+// Delete Todo by id
+app.delete('/todo/:id', (req, res) => {
+  const { id } = req.params;
+  todos = todos.filter((task) => task.id != id);
+  res.send(todos);
+});
